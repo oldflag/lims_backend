0da6bd050a442d81e2525ec01b3b4434909ded25
hk.lim <hklim@epigenome.us>
2023-04-24 10:37:06 -0700

    import specimen

--- controllers/project.js
+++ controllers/project.js
@@ -1,5 +1,7 @@
-import { createProject, getProjects, updateProject, deleteProject, getProject } from '../lib/projects.js';
+import { createProject, getProjects, updateProject, deleteProject, getProject, updateProjectWithSpecimens } from '../lib/projects.js';
 import tryCatch from './utils/tryCatch.js';
+
+//TODO: error handling
 
 export const createOne = tryCatch(async (req, res) => {
   const { id, name, type, description, status, metadata, collaboratorId } = req.body;
@@ -16,6 +18,11 @@ export const getAll = tryCatch(async (req, res) => {
 export const updateStatus = tryCatch(async (req, res) => {
   await updateProject(req.params.Id, req.body);
   res.status(200).json({ success: true, result: { id: req.params.Id } });
+});
+
+export const updateRelatedSpecimens = tryCatch(async (req, res) => {
+  let results = await updateProjectWithSpecimens(req.params.Id, req.body);
+  res.status(200).json({ success: true, result: { id: req.params.Id } }); 
 });
 
 export const deleteOne = tryCatch(async (req, res) => {
--- routes/projectRouter.js
+++ routes/projectRouter.js
@@ -1,9 +1,10 @@
 import { Router } from 'express';
-import { createOne, getAll, updateStatus, deleteOne } from '../controllers/project.js';
+import { createOne, getAll, updateStatus, deleteOne, updateRelatedSpecimens } from '../controllers/project.js';
 
 const projectRouter = Router();
 projectRouter.post('/register', createOne);
 projectRouter.get('/', getAll);
 projectRouter.patch('/updateStatus/:Id', updateStatus);
 projectRouter.delete('/:Id', deleteOne);
+projectRouter.patch('/updateRelatedSpecimens/:Id', updateRelatedSpecimens)
 export default projectRouter;
