4c165a7b2ed780adb3348c9dd2b99f53120fd56a
hk.lim <hklim@epigenome.us>
2023-04-26 11:13:12 -0700

    import quote

--- controllers/quote.js
+++ controllers/quote.js
@@ -1,4 +1,4 @@
-import { createQuote, getQuotes, updateQuote, deleteQuote, getQuote, createQuotes } from '../lib/quotes.js';
+import { createQuote, getQuotes, updateQuote, deleteQuote, getQuote, createQuotes, importQuote } from '../lib/quotes.js';
 import tryCatch from './utils/tryCatch.js';
 
 export const createOne = tryCatch(async (req, res) => {
@@ -12,6 +12,12 @@ export const createMany = tryCatch(async (req, res) => {
   // const { id, name, age, sex, ancestry, species, status, metadata, collaboratorId } = req.body;
   const results = await createQuotes(req.body)
   // const editedResults = await getQuote(results.id)
+  res.status(200).json({ success: true, result: results });
+});
+
+export const importOne = tryCatch(async (req, res) => {
+
+  const results = await importQuote(req.body)
   res.status(200).json({ success: true, result: results });
 });
 
--- routes/quoteRouter.js
+++ routes/quoteRouter.js
@@ -1,9 +1,10 @@
 import { Router } from 'express';
-import { createOne, getAll, updateStatus, deleteOne, createMany } from '../controllers/quote.js';
+import { createOne, getAll, updateStatus, deleteOne, createMany, importOne } from '../controllers/quote.js';
 
 const quoteRouter = Router();
 quoteRouter.post('/register', createOne);
 quoteRouter.post('/registerMany', createMany);
+quoteRouter.post('/import', importOne);
 quoteRouter.get('/', getAll);
 quoteRouter.patch('/updateStatus/:Id', updateStatus);
 quoteRouter.delete('/:Id', deleteOne);
